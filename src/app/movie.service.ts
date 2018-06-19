d604519c35235caad38a50985ee2f111a86c3998
Baskar Sikkayan <baskar.sks@gmail.com>
2018-06-18 21:53:08 -0700

    Adding environment for baseURI

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, retry } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
+import { environment } from './../environments/environment';
 
 
 
@@ -14,7 +15,7 @@ export class MovieService {
   // API: GET /getNowPlayingMovies
   public getNowPlayingMovies() {
     // will use this.http.get()
-    return this.http.get('https://api.themoviedb.org/3/movie/now_playing?api_key=e43d2d94b9a5b038b170a02ac2c9411e&language=en-US&page=4')
+    return this.http.get(environment.movieApiBaseUrl + 'now_playing?api_key=e43d2d94b9a5b038b170a02ac2c9411e&language=en-US&page=4')
       .pipe(
         retry(3), // retry a failed request up to 3 times
         catchError(this.handleError) // then handle the error
@@ -23,7 +24,7 @@ export class MovieService {
 
   public getUpcomingMovies() {
     // will use this.http.get()
-    return this.http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=e43d2d94b9a5b038b170a02ac2c9411e&language=en-US&page=4')
+    return this.http.get(environment.movieApiBaseUrl + 'upcoming?api_key=e43d2d94b9a5b038b170a02ac2c9411e&language=en-US&page=4')
       .pipe(
         retry(3), // retry a failed request up to 3 times
         catchError(this.handleError) // then handle the error
@@ -33,7 +34,7 @@ export class MovieService {
 
   public getMovieDetails(id) {
     // will use this.http.get()
-    const url = 'https://api.themoviedb.org/3/movie/' + id + '?api_key=e43d2d94b9a5b038b170a02ac2c9411e&language=en-US';
+    const url = environment.movieApiBaseUrl + id + '?api_key=e43d2d94b9a5b038b170a02ac2c9411e&language=en-US';
     return this.http.get(url)
       .pipe(
         retry(3), // retry a failed request up to 3 times
